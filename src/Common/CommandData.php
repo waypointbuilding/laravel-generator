b66baef82b026dae98cd88eb141f2a39e6bbd94b
jschlies <jimschlies@waypointbuilding.com>
2016-04-28 17:12:02 -0700

    getForeignKeys = deadcode

--- src/Common/CommandData.php
+++ src/Common/CommandData.php
@@ -104,10 +104,7 @@ class CommandData
             $this->getInputFromConsole();
         }
     }
-    public function getForeignKeys()
-    {
-        $this->getForeignKeysFromTable(); 
-    }
+    
     private function getInputFromConsole()
     {
         $this->commandInfo('Specify fields for the model (skip id & timestamp fields, we will add it automatically)');
@@ -265,11 +262,5 @@ class CommandData
 
         $this->inputFields = TableFieldsGenerator::generateFieldsFromTable($tableName);
         $this->checkForDiffPrimaryKey();
-    }
-    private function getForeignKeysFromTable()
-    {
-        $tableName = $this->dynamicVars['$TABLE_NAME$'];
-
-        $this->inputFields = TableFieldsGenerator::generateForeignKeysFromTable($tableName);
     }
 }
